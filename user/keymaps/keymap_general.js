e6643e2fa4f0adf3bbf6f5479af9e770c132f5ef
Dan Edens <Dan.Edens@Geo-instruments.com>
2020-05-03 17:03:34 -0500

    adds default keymap

---
+++ user/keymaps/keymap_general.js
@@ -0,0 +1,32 @@
+const keyMap = new Map()
+// keyMap.set('a', 'null')
+// keyMap.set('b', 'bookmarks')
+// keyMap.set('c', 'null')
+// keyMap.set('d', 'null')
+// keyMap.set('e', 'null')
+keyMap.set('f', 'find')
+// keyMap.set('g', 'null')
+// keyMap.set('h', 'null')
+// keyMap.set('i', 'null')
+// keyMap.set('j', 'null')
+// keyMap.set('k', 'null')
+// keyMap.set('l', 'null')
+// keyMap.set('m', 'mouseMode')
+// keyMap.set('n', 'newTab')
+// keyMap.set('o', 'null')
+// keyMap.set('p', 'null')
+// keyMap.set('q', 'null')
+// keyMap.set('r', 'null')
+// keyMap.set('s', 'null')
+// keyMap.set('T', 'null')
+// keyMap.set('u', 'null')
+// keyMap.set('v', 'null')
+// keyMap.set('w', 'close')
+// keyMap.set('x', 'null')
+// keyMap.set('y', 'null')
+// keyMap.set('z', 'null')
+export keyMap
+
+
+
+
